8006338f9a967c08f06905e4dff12c5cf4e92ff5
tatsuo48 <yokoyama.tatsuo@mwed.co.jp>
2020-05-10 21:20:22 +0900

    init

---
+++ config.js
@@ -0,0 +1,46 @@
+module.exports = {
+    url: "http://tatsuo48.me",
+    title: "おやかたの話",
+    lang: "ja",
+    description: "",
+    defaultKeywords: [`blog`, `gatsby`, `javascript`, `aws`, `tatsuo48`],
+    defaultMetaImage: "/defaultmetaimage.png",
+
+    authorNickName: "tatsuo48",
+    authorFullName: "YokoyamaTatsuo",
+    authorDescription: "HameeのSRE, 日々の諸々を書きます。",
+    photo: "content/assets/my-pic.jpg",
+
+    // icon : FontAwesome
+    category: [
+        {
+            id: "Cat",
+            icon: "fas fa-cat",
+        },
+        {
+            id: "Job",
+            icon: "fas fa-building",
+        },
+        {
+            id: "Tech",
+            icon: "fas fa-wrench",
+        },
+        {
+            id: "Life",
+            icon: "fas fa-heart",
+        },
+    ],
+
+    social: {
+        twitter: "https://twitter.com/tatsuo4848",
+        facebook: "https://www.facebook.com/tatsuo48",
+        github: "https://github.com/tatsuo48",
+        email: "green.americanidiot.yokoyama@gmail.com",
+    },
+
+    postsPerPage: 5,
+    pageListSize: 5,
+
+    copyright: "© All rights reserved.",
+    googleAnalyticsId: "HOGEHOGEUA-143311992-2",
+};
---
+++ src/themes/light.js
@@ -0,0 +1,62 @@
+export default {
+    main: {
+        defaultBack: "#fff",
+        defaultText: "#000",
+        overlayBack: "#000",
+        header: {
+            back: "#16a4b6",
+            text: "#fff",
+            button: "#fff",
+        },
+    },
+    side: {
+        defaultBack: "#16a4b6",
+        defaultText: "#fff",
+        bio: {
+            circle: "#49c39e",
+            defaultText: "#fff",
+            activeText: "#000",
+        },
+        category: {
+            activeText: "#000",
+            activeBack: "#49c39e",
+        },
+    },
+    bio: {
+        defaultBack: "#fff",
+        defaultText: "#000",
+        subText: "#999",
+        socialButton: "#000",
+    },
+    postlistitem: {
+        title: "#000",
+        content: "#999",
+        info: "#000",
+    },
+    pagination: {
+        defaultText: "#999",
+        activeText: "#fff",
+        activeBack: "#16a4b6",
+    },
+    blogpost: {
+        title: "#000",
+        info: "#999",
+        hr: "#999",
+        content: {
+            default: "#000",
+            quote: "#999",
+            link: "#0687f0",
+        },
+    },
+    tag: {
+        back: "#d9d9d9",
+        text: "#4d4d4d",
+    },
+    recentpostlist: {
+        header: "#000",
+        category: "#0687f0",
+    },
+    recentpostitem: {
+        text: "#fff",
+    },
+};
